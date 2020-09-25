3e41c1823c483c5584259ecaa8a39afd0048a43a
hdinh77 <heatherdinh11@gmail.com>
2020-09-25 00:31:08 -0700

    updates with React UI

--- tennis-weather-app/src/App.js
+++ tennis-weather-app/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import './config';
+import config from './config.js';
 
 const api = {
-  key: config.API_KEY,
+  key: "2e0a2e3d27a24770ecdcb29b43c6b860",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
@@ -18,7 +18,7 @@ function App() {
         .then(res => res.json())
         .then(result => {setWeather(result); setQuery(''); console.log(result);});
     }
-  }
+  } 
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -34,6 +34,7 @@ function App() {
 
   const timeBuilder = (d) => {
     let hour = ('0' + d.getHours()).slice(-2) % 12;
+    if(hour == '0') hour = '12';
     let minutes = ('0' + d.getMinutes()).slice(-2);
 
     return `${hour}:${minutes}`;
@@ -50,6 +51,7 @@ function App() {
           <div className="search-box">
             <input type="text" className="search-bar" placeholder="Search..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/>
           </div>
+          
           {(typeof weather.main != "undefined") ? (
             <div>
               <div className="location-box">
@@ -60,20 +62,20 @@ function App() {
 
               <div className="weather-box">
                 <div className="temp">{Math.round(weather.main.temp)}°F</div>
-                <div className="weather">{weather.weather[0].main}</div>
+                <div className="tennis-condition">Condition for Tennis: </div>
               </div>
               <div className="details-box">
-                <div className="humidity"></div>
-                <div className="pressure"></div>
-                <div className="wind"></div>
-                <div className="rain"></div>
+                <div className="weather">Weather: {weather.weather[0].main}</div>
+                <div className="humidity">Humidity: {weather.main.humidity}%</div>
+                <div className="wind">Wind: {Math.round(weather.wind.speed)}mph</div>
+                <div className="feels-like">Feels like: {Math.round(weather.main.feels_like)}°F</div>
               </div>
             </div>
           ) 
           : (
             <div>
               <div className="home">
-                  <div className="welcome">How is the weather for tennis?</div>
+                  <div className="welcome">TennisTemp</div>
                   <div className="date">{dateBuilder(new Date())}</div>
               </div>
             </div>
