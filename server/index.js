eb0bdaa8c4c9f4b34d2c4897c174f997432927c8
hdinh77 <heatherdinh11@gmail.com>
2020-09-28 22:15:13 -0700

    finishing server with sql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const db = mysql.createPool({
     host: 'localhost',
@@ -10,15 +12,38 @@ const db = mysql.createPool({
     port: '3306'
 });
 
+/*
 app.get("/", (req, res) => {
     const sqlInsert = "INSERT INTO tennis_weather.previous_calls (city, temp) VALUES ('seattle', '20');";
     db.query(sqlInsert, function (err, result) {
         if(err) console.log(err);
         res.send(sqlInsert);
     });
-}); 
+});
+*/
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/get', (req, res) => {
+    const sqlSelect = "SELECT * FROM tennis_weather.previous_calls";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+})
+
+app.post('/', (req, res) => {
+    const city = req.body.city;
+    const temp = req.body.temp;
+    const sqlInsert = "INSERT INTO tennis_weather.previous_calls (city, temp) VALUES (?, ?)";
+    db.query(sqlInsert, [city, temp], (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+});
 
 app.listen(3001,() => {
     console.log('running on port 3001');
 
-})
+});
